c3cb4c3d58c0d9b464c3349e2e997d64df57918f
ANKIT128912 <ankit121ab@gmail.com>
2026-03-05 12:22:20 +0530

    fix cors issue

--- server.js
+++ server.js
@@ -11,19 +11,16 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://frontened-4zs9.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// Enable CORS
+app.use(cors());
 
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
 
+// Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
